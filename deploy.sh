a2ba1b77e19071c1cfd82ae48ee60018a1cfedc9
Flor Holvoet <holvoetf@gmail.com>
2019-06-09 18:29:16 -0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-USER=root
-HOST=server1.crays.ee
-DIR=/var/www/vhosts/florholvoet.be/httpdocs/   # might sometimes be empty!
-
-hugo && rsync -avz --delete public/ ${USER}@${HOST}:${DIR}
-
-exit 0
